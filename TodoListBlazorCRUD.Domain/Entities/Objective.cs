c9e00b7f35f552fc508bdd43f1eb5a6ec98eb385
Rainerre Galendez <rainerregalendez@outlook.com>
2024-04-19 23:45:45 +0800

    Added Objective Card, with css, implemented Add in Objective

--- TodoListBlazorCRUD.Domain/Entities/Objective.cs
+++ TodoListBlazorCRUD.Domain/Entities/Objective.cs
@@ -1,15 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using TodoListBlazorCRUD.Domain.Enum;
 
 namespace TodoListBlazorCRUD.Domain.Entities
 {
     public class Objective
     {
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage ="Please provide a title")]
         [StringLength(50)]
         public string? Title { get; set; }
+
+        [Required(ErrorMessage ="Please provide a due date")]
         public DateTime? DueDate { get; set; }
-        public int Priority { get; set; }
+
+        [EnumDataType(typeof(Priority),ErrorMessage ="Please select a Priority")]
+        public Priority Priority { get; set; }
+
         public bool IsDone { get; set; }
     }
 }
